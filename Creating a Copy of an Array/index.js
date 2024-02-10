e24f357e59b761e6d2b2d902f00065eb69c82f50
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-10 21:00:33 +0530

    Create index.js

---
+++ Creating a Copy of an Array/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let myArray = JSON.parse(readLine().replace(/'/g, '"'));
+/* Please do not modify anything above this line */
+
+  let newArray = [...myArray];// Write your code here.
+  
+/* Please do not modify anything below this line */  
+  console.log(newArray);
+}
