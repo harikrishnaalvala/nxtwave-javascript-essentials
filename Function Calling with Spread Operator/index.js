8a5ed6d91a0d2d77b2991771d75af73e45e92994
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-10 21:01:30 +0530

    Create index.js

---
+++ Function Calling with Spread Operator/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function getProductOfTheIntegers(...args) {
+  let productOfAllValues = 1;
+  for (let value of args) {
+    productOfAllValues = productOfAllValues * value;
+  }
+  
+  return productOfAllValues;
+}
+
+function main() {
+  let integersArray = JSON.parse(readLine().replace(/'/g, '"'));
+/* Please do not modify anything above this line */
+
+  
+  console.log(getProductOfTheIntegers(...integersArray));
+  
+  /*
+ 
+   * Write your code here and log the output.
+   */
+}
+
