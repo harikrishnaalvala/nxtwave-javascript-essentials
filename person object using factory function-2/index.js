cff0a933474bb9b4ffc7388fae10e3e8cca9f17e
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-11 11:49:40 +0530

    Create index.js

---
+++ person object using factory function-2/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function createPerson(firstName, lastName) {
+    firstName,
+    lastName
+  /* Write your code here. */
+}
+
+function main() {
+  const firstName = readLine();
+  const lastName = readLine();
+ let fullName= createPerson(firstName,lastName)
+ console.log(`${firstName} ${lastName}`)/* Write your code here. */
+}
