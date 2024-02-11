28ea5125fe63d0e04318184e95c5c34d1d6b2fc9
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-11 11:41:16 +0530

    Create index.js

---
+++ quote/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+   let  quote= () => "Make it work, make it right, make it fast."
+   console.log(quote())
+  /* Please do not modify anything above this line */
+   
+  /*
+   * Write the arrow function here and log the output.
+   */
+}
