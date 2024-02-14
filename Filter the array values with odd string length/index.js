081139ae9e9ff3f3010fe5eb2cc8ba9e806c4ac6
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-14 19:18:39 +0530

    Create index.js

---
+++ Filter the array values with odd string length/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function main() {
+  const myArray = JSON.parse(readLine().replace(/'/g, '"'));
+ const filter=myArray.filter((number)=>
+ number.length%2==1)
+ const mapFunction=filter.map((word)=>
+ word.toUpperCase()
+) 
+console.log(...mapFunction)/* Write your code here */
+}
