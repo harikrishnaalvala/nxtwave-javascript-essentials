34a781708f973b76147a3b808bd93ecc5774b369
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-14 19:15:12 +0530

    Create index.js

---
+++ Filter the array values with even string length/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function main() {
+  const myArray = JSON.parse(readLine().replace(/'/g, '"'));
+  const filterArray=myArray.filter((word)=>
+   word.length%2==0
+  )
+  console.log(...filterArray)
+  /* write your code here */
+}
